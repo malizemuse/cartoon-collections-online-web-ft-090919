44de372577e058d2fec75b3120151523cea36a9b
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-15 16:20:14 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -34,7 +34,7 @@ end
 # TEST CODE
 
 short_words = ["puff", "go", "two"]
-long_planeteer_calls(short_words)
+puts long_planeteer_calls(short_words)
 
 assorted_words = ["two", "go", "industrious", "bop"]
 long_planeteer_calls(assorted_words)
