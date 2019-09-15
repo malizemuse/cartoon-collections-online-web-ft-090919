1cceaafff636cdda5643c389a4bba17861f12f30
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-15 16:33:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -45,12 +45,12 @@ end
 # TEST CODE
 
 snacks = ["crackers", "gouda", "thyme"]
-find_the_cheese(snacks)
+puts find_the_cheese(snacks)
 
 soup = ["tomato soup", "cheddar", "oyster crackers", "gouda"]
-find_the_cheese(soup)
+puts find_the_cheese(soup)
 
 ingredients = ["garlic", "rosemary", "bread"]
-find_the_cheese(ingredients)
+puts find_the_cheese(ingredients)
 
 
