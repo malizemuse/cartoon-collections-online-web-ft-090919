a9c4a8931e24acef23fc1eb10b84d24ff674ac7c
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-15 16:18:07 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,15 +8,15 @@ end
 
 def summon_captain_planet(array)
   
-  array.map do |call|
+  array.map { |call|
     call.capitalize! + "!"
-  end 
+  } 
   
 end
 
 def long_planeteer_calls(array)
   
-  
+  array.any? d
   
   
 end
