23de6cc312ff0625ce353b964b4e04bc5c710369
gregarious-number-3828 <muse.malize@gmail.com>
2019-09-15 16:36:28 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,42 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+def roll_call_dwarves(array)
+  
+  array.each_with_index { |dwarf, i|
+    puts "#{i + 1}. #{dwarf}"
+  }
+  
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(array)
+  
+  array.map { |call|
+    call.capitalize! + "!"
+  } 
+  
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(array)
+  
+  array.any? { |call|
+    call.length > 4 
+  } 
+  
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(array)
+  
   cheese_types = ["cheddar", "gouda", "camembert"]
+  
+  array.find do |snack|
+    
+    i = 0 
+    while i < cheese_types.length 
+      return snack if snack == cheese_types[i]
+      i += 1 
+    end 
+    
+  end 
+  
+  
 end
+
+
